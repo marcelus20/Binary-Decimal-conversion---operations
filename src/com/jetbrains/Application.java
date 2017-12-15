12a5cd413a9f7d019223d28a7a14bd4acb3e51b8
Felipe <marcelus20felipe@gmail.com>
2017-12-15 19:14:14 +0000

    I made Auxiliar class inherit from Operations, therefore Application has multiple inheritance as it inherit from Auxiliar

--- src/com/jetbrains/Application.java
+++ src/com/jetbrains/Application.java
@@ -1,6 +1,6 @@
 package com.jetbrains;
 
-public class Application extends Auxiliar {
+public class Application extends Auxiliar{
 
     void welcome(){
         String[] welcome = new String[]{"WELCOME ","TO THE","BINARY","CALCULATOR"};
--- src/com/jetbrains/Auxiliar.java
+++ src/com/jetbrains/Auxiliar.java
@@ -6,7 +6,7 @@ THIS CLASS WAS CREATED TO HELP OPERATE OTHER CLASSES.
 IT WILL HAVE METHODS THAT WILL BE CALLED IN OTHER CLASSES
 
 */
-public class Auxiliar{
+public class Auxiliar extends Operations{
 
     //THIS FUNTION WILL REPLACE THE system.out.println() method
     void print(Object stuff){
