64e0c9e7e521b9942ae484f44144938f01ee7a0b
Felipe <marcelus20felipe@gmail.com>
2017-12-15 19:10:49 +0000

    Created the operation class, it will be responsible to carry out the operations.

---
+++ src/com/jetbrains/Operations.java
@@ -0,0 +1,12 @@
+package com.jetbrains;
+
+public class Operations {
+    void sum (String a, String b){}
+    void sub (String a, String b){}
+    void mult(String a, String b){}
+    void dev (String a, String b){}
+
+    //CONVERTIONS
+    void bin_to_dec(String number){}
+    void dec_to_bin(String number){}
+}
