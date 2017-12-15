087388e92233518a65724c690565faf256af7b14
Felipe <marcelus20felipe@gmail.com>
2017-12-15 18:37:55 +0000

    welcome message was done, it's calling the arr_printer method for printing the message in a nice way

--- src/com/jetbrains/Application.java
+++ src/com/jetbrains/Application.java
@@ -1,10 +1,15 @@
 package com.jetbrains;
 
 public class Application extends Auxiliar {
-    void menu(){
-
-    }
     void welcome(){
+        String[] welcome = new String[]{"WELCOME",
+                                      "TO THE",
+                                        "BINARY",
+                                        "CALCULATOR"
+                                        };
+        arr_printer(welcome);
+    }
+    void menu(){
 
     }
 }
--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -1,11 +1,12 @@
 package com.jetbrains;
 
-public class Main extends Auxiliar{
+public class Main extends Application{
 
     Main(){
         //print(number_catcher("", "", ""));
-        String[] a = new String[]{"hello", "How are you", "yay"};
-        arr_printer(a);
+        //String[] a = new String[]{"hello", "How are you", "yay"};
+        //arr_printer(a);
+        welcome();
     }
 
     public static void main(String[] args) {
